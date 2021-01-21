135174c77707b54d4e5ac7d735da675b8120f76c
t.claude94 <23203061+tclaude94@users.noreply.github.com>
2021-01-21 17:41:42 +0100

    Ajout route dynamique API

---
+++ pages/api/server/[id].js
@@ -0,0 +1,42 @@
+import temp from "../../../database/temp.json";
+import { getSession } from "next-auth/client";
+
+export default async (req, res) => {
+  const session = await getSession({ req });
+  if (session) console.log("Session", JSON.stringify(session, null, 2)); // Pour voir la struct des infos recues
+  const requestedServer = temp[req.query.id] || []; // Récupère les infos d'un serveur
+
+  // Dég mais gère 2 fonctionnement diff rapidement
+  if (["288659194737983489", "2"].includes(req.query.id)) {
+    if (!session) return res.status(401).end();
+
+    /*
+       La requete vient elle d'un admin ? Ici j'utilise un tableau avec les id d'admin dedans mais on peut imaginer un appel à l'API Discord (via discordjs / erisjs)
+    */
+    if (!requestedServer.admins.includes(session.user.id))
+      return res.status(403).end();
+
+    res.status(200).json({ queryId: req.query.id, requestedServer });
+  } else {
+    res
+      .status(200)
+      .json({ isLogged: !!session, queryId: req.query.id, requestedServer });
+  }
+};
+
+/*
+
+pour gérer les différentes méthodes HTTP :
+
+export default (req, res) => {
+  switch (req.method) {
+    case 'GET':
+      break
+    case 'POST':
+      break
+    default:
+      res.status(405).end()
+      break
+  }
+}
+*/
