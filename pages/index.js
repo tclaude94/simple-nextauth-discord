a13bb54a75bcab5c3fdb82e4422c143c3b50f7f8
t.claude94 <23203061+tclaude94@users.noreply.github.com>
2021-01-21 17:41:19 +0100

    Affiche bouton login / servs qd connecté

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,32 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import { signIn, signOut, useSession, getSession } from "next-auth/client";
+
+import useSWR from "swr";
+
+const fetcher = (url, accessToken) =>
+  fetch(url, {
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+  }).then(async (res) => {
+    if (res.status !== 200) throw new Error("Problème API");
+    const guilds = res.json();
+    return guilds;
+  });
 
 export default function Home() {
+  const [session, loading] = useSession();
+  const { data: guilds, error } = useSWR(
+    !loading && session
+      ? ["https://discord.com/api/users/@me/guilds", session.user.accessToken]
+      : null,
+    fetcher,
+    {
+      revalidateOnFocus: false,
+    }
+  );
+
   return (
     <div className={styles.container}>
       <Head>
@@ -14,52 +39,40 @@ export default function Home() {
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+        {!session && (
+          <>
+            <p>Salut t'es pas log</p>
+            <button onClick={() => signIn("discord")}>Login discord</button>
+          </>
+        )}
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+        {session && (
+          <>
+            <p>Salut t'es log</p>
+            <button onClick={() => signOut()}>Logout</button>
+          </>
+        )}
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        {!guilds && !error && session && <p>Chargement ...</p>}
+        {error && <p>{error.message} retry en cours ...</p>}
+        {guilds && (
+          <div>
+            {guilds.map((guild, id) => (
+              <div style={{ display: "inline-flex" }} key={id}>
+                <img
+                  width="60px"
+                  height="60px"
+                  src={`https://cdn.discordapp.com/icons/${guild.id}/${guild.icon}.webp`}
+                  style={{ borderRadius: "50%" }}
+                />
+                <p>
+                  {guild.name} {guild.owner ? "(owner)" : ""}
+                </p>
+              </div>
+            ))}
+          </div>
+        )}
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
-  )
+  );
 }
